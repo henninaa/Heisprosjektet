327a756efb2c27bd0bbe48309c6d04a55a7847a8
baardrb <baardrb@stud.ntnu.no>
2015-04-20 11:49:21 +0200

    Nettverk skal funke

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,13 +1,24 @@
 package main
 
-import . "network_module"
+import (
+		"network_module"
+		"time"
+		)
 
 func main(){
-	var NetChan NetChannels
+	var NetChan network_module.NetChannels
 
-	go Start_network(NetChan)
+	//go network_module.Network_external_chan_init()
+	go network_module.Start_network(NetChan)
 
 	deadChan := make(chan int)
 	<-deadChan
 }
 
+func repeater(netChan network_module.NetChannels){
+	for {
+		mail := network_module.Mail{Msg: []byte("TST")}
+		netChan.SendToAll <- mail
+		time.Sleep(1*time.Second)
+	}
+}
